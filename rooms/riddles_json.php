66baac65862ec41b1e77a9a543faddeaf17a6ba8
ReyaanAli9028405 <9028405@student.tcrmbo.nl>
2026-03-26 13:34:08 +0100

    w

--- rooms/riddles_json.php
+++ rooms/riddles_json.php
@@ -1,13 +1,20 @@
-<?php
-require_once('../dbcon.php');
+
+ <?php
 header('Content-Type: application/json; charset=utf-8');
 
-try {
-    $stmt = $db_connection->prepare("SELECT riddle, answer FROM riddles WHERE roomId = 1");
-    $stmt->execute();
-    $riddles = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    echo json_encode($riddles, JSON_UNESCAPED_UNICODE);
-} catch (PDOException $e) {
-    http_response_code(500);
-    echo json_encode(['error' => 'Databasefout: ' . $e->getMessage()]);
-}
+$riddles = [
+    [
+        'riddle' => 'Ik hang aan de muur, ik piep als je me aanraakt, en ik laat zien of je nog leeft. Wat ben ik?',
+        'answer' => 'hartmonitor'
+    ],
+    [
+        'riddle' => 'Ik ben klein, scherp en iedereen is bang voor mij, maar zonder mij kun je niet genezen. Wat ben ik?',
+        'answer' => 'naald'
+    ],
+    [
+        'riddle' => 'Ik zie alles, zelfs wat onder je huid zit, maar ik heb geen ogen. Wat ben ik?',
+        'answer' => 'röntgenapparaat'
+    ]
+];
+
+echo json_encode($riddles, JSON_UNESCAPED_UNICODE);
